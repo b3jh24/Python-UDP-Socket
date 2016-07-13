13d179005a432b3a68affe60b1411328b694dc70
BHornak <b3jh24@gmail.com>
2016-07-13 17:11:28 -0400

    first commit

---
+++ MoreExperimentalRecv.py
@@ -0,0 +1,221 @@
+'''
+Created on Jun 16, 2016
+
+
+@author: bhornak
+'''
+import logging
+import socket
+import time
+import collections
+import threading
+import os
+
+UDP_IP = "10.102.46.218"  # Address of the guy (me) hosting the UDP socket (in this case it's Andrew) 
+UDP_PORT = 5005
+buf = 8972  # Buffer size in bytes for each chunk
+
+TCP_IP = "10.102.46.218"  # Address of the guy hosting the TCP socket (in this case it's Brian)
+
+actualNumChunks = 0
+
+tcp_port = 7007
+tcp_buf = 1024
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+'''
+Create TCP socket to make basic communication requests
+'''
+# FIXME: Need to add socket.SO_REUSEADDR to allow socket to be reused, but this needs privileges
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_sock.connect((TCP_IP, tcp_port))
+tcp_sock.send("GET_NUM_CHUNKS")
+numChunksToRecv = tcp_sock.recv(tcp_buf)  # The number of chunks we should get by the end of this thing
+print "Expecting to receive ", numChunksToRecv  # --KEEP
+#Convert numChunksToRecv to an int (from a string)
+numChunksToRecv = float(numChunksToRecv)
+numChunksToRecv = int(numChunksToRecv)
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 30485760)
+numRecv = 0  # The number of chunks we've received so far (reset after each ACK)
+totalNumRecv = 0  # The total number of chunks we've received so far, never resets. Used to know when we've got everything
+canRecv = False
+f = open('expRecv2.txt', 'wb')
+numChunksRequested = 8000  # The number of chunks we're gonna ask for
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+'''
+Request that the server send us some chunks - we're ready for it
+Do this via a TCP stream    
+'''
+tcp_sock.send("Send me some chunks");
+if(tcp_sock.recv(tcp_buf)):
+    # We got a response from the server, let's get ready for those chunks
+    print "Server about to send some chunks"
+    canRecv = True
+
+
+totalNumLoops = numChunksToRecv / numChunksRequested        #The total number of loops we expect to go through. In essence, this is the number of ACKs we will end up sending
+totalNumBytesToRecv = numChunksToRecv * buf                 #The total number of bytes we expect to receive by the end of the transfer
+progressMultiplier = 1                                      #Range 1-20: Used for figuring out how much data we've received as a percentage of the total amount we should expect
+                                                            #Every 5% we will notify
+
+def write_to_file(path, buffer, terminate_signal):
+    """Write the datagram chunks to a file in a separate thread, so long as data is provided
+    
+    Args:
+        path - the location of the file to write to
+        buffer - the list (if you will) containing the data to be written
+        terminate_signal - an indicator that we've finished receiving data and do no not expect more data to write
+    """
+    with open(path, 'wb') as out_file:  # close file automatically on exit
+        while not terminate_signal.is_set() or buffer:  # go on until end is signaled
+            try:
+                data = buffer.pop()  # pop from RIGHT end of buffer
+            except IndexError:
+                time.sleep(0.5)  # wait for new data
+            else:
+                out_file.write(data)  # write a chunk
+    print "File closed"
+
+def RecvResentPackets(f, numBeingResent):
+    """Receive packets that were resent (separate from initial receiving loop.
+    
+    Args:
+        f - file-like object to write resent packets to
+        numBeingResent- the number of packets that will be resent (so we can know how many we should be expecting)
+    
+    Return:
+        1 - on successfully receiving all resent packets
+        0 - we failed to get all of the data that was resent
+    """
+    numResentRecv = 0
+    try:
+        while(canRecv):
+            sock.settimeout(2)
+            dataChunk, addr = sock.recvfrom(buf)
+            packID, junkAddr = sock.recvfrom(buf)
+            if(packID.isdigit() and dataChunk):
+                numResentRecv += 1
+                global totalNumRecv
+                totalNumRecv += 1
+                print "Resent PackID: " + packID
+                '''
+                Once we've received an ID, we want to store it in a list. We put it in the index that corresponds to it's ID number
+                and flag it as received (with a 1). Once we've timed out, we can check this list to see what we're missing
+                '''
+                chunkIDsRecv[int(packID)] = 1
+                #f.write(dataChunk)
+            else:
+                logging.warning("The resend got fucked up...")
+            if(numResentRecv == numBeingResent):
+                # We've got the packets we asked for, let's alert the server to this fact
+                tcp_sock.send("I got the 60 packets I requested, you can send some more")
+                print "We got the " + str(numResentRecv) + " resent packets"
+                return 1
+    except socket.timeout:
+        print "We didn't get all the packets that were resent, gotta get them back somehow"
+        print "But we did get a total of " + str(totalNumRecv) + " chunks ("+str(numResentRecv)+" were resent packets)"
+        return 0
+
+
+chunkIDsRecv = [0] * (numChunksRequested)  # List of IDs received 
+'''
+The  number of loops we've gone through. A loop is defined as every time we've gotten as many packets as we've asked for (either received originally, or retransmitted).
+Essentially, a completed loop is when we send an ACK. We use this to keep store things in lists properly because ID numbers don't necessarily correspond to list indices after the first
+loop/round
+'''  
+loop = 0
+buffer = collections.deque()  # buffer for reading/writing
+terminate_signal = threading.Event()  # shared signal
+threads = [
+    threading.Thread(target= write_to_file, kwargs=dict(
+    path="t1.txt",
+    buffer=buffer,
+    terminate_signal=terminate_signal
+  ))]
+threads[0].start()
+
+try:
+    while(canRecv):
+        sock.settimeout(2)
+        dataChunk, addr = sock.recvfrom(buf)
+        packID, junkAddr = sock.recvfrom(buf)
+        if(packID.isdigit() and dataChunk):
+            numRecv += 1
+            totalNumRecv += 1
+            #print "TotalNumRecv: " , totalNumRecv
+            '''
+            Once we've received an ID, we want to store it in a list. We put it in the index that corresponds to it's ID number
+            and flag it as received (with a 1). Once we've timed out, we can check this list to see what we're missing
+            '''
+            chunkIDsRecv[int(packID) - (loop*numChunksRequested)] = 1
+            buffer.appendleft(dataChunk)  # append to LEFT of buffer
+            
+            '''
+            Provide a progress/status update to the operator
+            TODO: We also want to let the server know that we've gotten this much data too
+            '''
+            if(totalNumRecv*buf >= progressMultiplier * .05 * totalNumBytesToRecv):
+                print str(progressMultiplier*5) + "% of data received"
+                progressMultiplier += 1
+        else:
+            logging.warning("Something got fucked up - either lost data packet, or missing ID number")
+       
+        if(numRecv == numChunksRequested):
+            # We've got the packets we asked for, let's alert the server to this fact
+            tcp_sock.send("I got the 60 packets I requested, you can send some more")
+            #print "We got " + str(numRecv) + " packets"
+            numRecv = 0
+            loop += 1
+            chunkIDsRecv = [0] * numChunksRequested     #Clear out the cache
+            continue
+        if(totalNumRecv == int(numChunksToRecv)):
+            # We've got all the chunks we were supposed to get for the whole transfer, let's close the socket and files
+            f.close()
+            sock.close()
+            tcp_sock.close()
+            print "File received, we got " + str(totalNumRecv) + " chunks"
+            time.sleep(.5)
+            gb = round(os.path.getsize("t1.txt")/10e8,2)
+            tb = round(os.path.getsize("t1.txt")/10e11,4)
+            print "Total data: " + str(os.path.getsize("t1.txt")) + " bytes = " + str(gb) + "GB = " + str(tb) + "TB"
+            terminate_signal.set()
+            canRecv = False 
+except socket.timeout:
+    '''
+    Something happened, and we didn't get all the packets we requested in time (we timed out).
+    Let's not let the server panic, and tell him what happened,and how many packets we got so far.
+    '''
+    print "Timed out, but have no fear, we got " + str(numRecv) + " chunks"
+    # The number of chunks I'm still waiting on
+    numINeed = numChunksRequested - numRecv    
+    canRecv = False
+    terminate_signal.set()  # signal writer that we are done
+    
+    '''
+    Now we have to figure out what packets we're missing (by checking our list), and telling the server
+    to send us those missing packets before we can be get move on to the next round
+    
+    The packets we want are packets from our list that don't have a 1 as their list value
+    Remember, the list is as such: Index: ID_NUM    Value: 1/0    
+    '''
+    packetsINeed = []
+    for i in xrange(0, len(chunkIDsRecv)):
+        if chunkIDsRecv[i] is 0:
+            packetsINeed.append(i)
+    
+    print "Packets I need: " + str(packetsINeed) + " -- Length: "+str(len(packetsINeed)) + "\n"
+    tcp_sock.send(str(packetsINeed))
+    canRecv = True
+    if(RecvResentPackets(f, len(packetsINeed))):
+        #TODO: Figure out how to go back to the main receiving loop
+        #We successfully received all the data that was resent, go back to the main receiving loop
+        print "YAY!"
+    else:
+        #TODO: Figure out what we do with the missing resent data?
+        #We didn't get all of the resent data, now what the fuck do we do?
+        #print "What the fuck do I do now??"
+        print "FIXME"
+    
---
+++ MoreExperimentalSending.py
@@ -0,0 +1,195 @@
+'''
+Created on Jun 15, 2016
+
+@author: bhornak
+'''
+#from scapy.all import *
+import socket
+import os
+import math
+import time
+import logging
+from __builtin__ import int
+from time import sleep
+
+buf = 8972
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+host ="10.102.46.218"   #Address of the guy hosting the UDP socket - in this case it's Andrew
+port = 5005
+addr = (host,port)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_SNDBUF, 30485760)
+
+
+tcp_host = "10.102.46.218"  #Address of the guy (me) hosting the TCP socket - in this case it's Brian
+tcp_port = 7007
+tcp_buf = 1024
+
+packetQueue = [None] * 8000        #Empty list that contains each packet sent - used for retransmissions (can only contain the
+                                 #number of packets that the client requested (after that, it dumps everything)
+
+f = open('file.txt', 'rb')
+
+def getNumChunksToSend():
+    """Returns the number of chunks that will be sent to the client
+    The client will use this to know how many blocks it should receive (useful for data verification)
+    """
+    #TODO change file name from file.txt to be whatever the data/file actually is
+    size = os.path.getsize("file.txt")
+    numToRound = float(size)/float(buf)  #Use float point math because int's wont give correct number
+    return math.ceil(numToRound)         #File size/chunk size = number of chunks - rounded up
+
+'''
+Create a TCP socket to enable communication between server and client
+'''
+#FIXME: Need to add socket.SO_REUSEADDR to allow socket to be reused, but this needs privileges
+TCP_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+TCP_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+TCP_sock.bind((tcp_host, tcp_port))
+TCP_sock.listen(1)
+
+conn, tcp_addr = TCP_sock.accept()
+conn.settimeout(1)
+while 1:
+    clientReq = conn.recv(tcp_buf)
+    if not clientReq: break
+    #print "Client request: ", clientReq
+    if (clientReq == "GET_NUM_CHUNKS"):
+        conn.send(str(getNumChunksToSend()))
+        #print "Sent GET_NUM_CHUNKS"
+        break
+    else:
+        print "Command not recognized"
+        break
+#conn.close()
+#TCP_sock.close()
+
+def read_in_chunks(infile, chunk_size=buf):
+    """Chunk the file before we send it.
+    
+    Args:
+        infile -- the file to chunk
+        chunk_size -- the size of the chunk in bytes (using 512 b/c thats what the FPGA uses)
+    """
+
+    while True:
+        chunk = infile.read(chunk_size)
+        if chunk:
+            yield chunk
+        else:
+            # The chunk was empty, which means we're at the end of the file
+            return
+
+def run():
+    local_ID = 0
+    totalNumSent = 0
+    numSent = 0
+    loop = 0
+    howManyToSend = 8000      #Number of chunks the client wants us to send - hardcoded for now
+    conn.settimeout(None)
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    
+    '''
+    First, we need to get some basic info from the client.
+    Let's see if he's ready to receive data, and if so, how much does he want?
+    '''
+    if(conn.recv(tcp_buf) == "Send me some chunks"):
+        logging.info("He wants us to send him 60 chunks of data, let's do it!")
+        conn.send("Chunks on their way brah!")
+    else:
+        logging.warning("Uh oh, something went wrong!")
+    
+    '''
+    How the hell this works:
+    
+    We want to only send as many packets (chunks) as the client asks for, no more no less. However, we want to make sure that each packet is a new data chunk (not the same one over
+    and over again like I had in the old code). So, we loop through our file, pulling out chunks as we go along. Once we pull out that chunk, and before we send it 
+    we check to see if we've sent as many chunks as the client wanted. If we have, we wait for an ACK, and proceed accordingly. Then, after we've clarified that the client is ready 
+    for more, we send the current chunk we pulled out. Then we go to the next chunk. Once we get to the last group of data to be sent (a group being all the chunks we send before an ACK)
+    then signal that this is the last group we're gonna send, and finish off the transfer.
+    '''
+    keepGoing = True
+    start = time.time()
+    while(totalNumSent < getNumChunksToSend() and keepGoing):
+        while(numSent < howManyToSend and keepGoing):
+            if(totalNumSent > getNumChunksToSend() - howManyToSend):
+                    print "Total Num Sent: ",totalNumSent
+                    print "Num Sent: ",numSent
+                    keepGoing = False
+            for chunk in read_in_chunks(f):               
+                if(numSent == howManyToSend):
+                    '''
+                    We've done our part, and sent the number of chunks he wanted, now we wait to hear from him
+                    We should expect to hear from him, because once he times out (i.e. after we sent our data, and he stopped receiving it)
+                    he'll at least tell us what went wrong
+             
+                    The ACK will either be something of the form "I got everything you sent me, send more", or "These are the packets I didn't get"
+                    '''
+                    ACK = conn.recv(tcp_buf)
+                    #print "ACK: "+ACK
+                    if(ACK != "I got the 60 packets I requested, you can send some more"):
+                        '''
+                        He didn't get all 60 packets, but he *probably* told us what he did get, and that's good enough
+                        We just need to figure out how many we need to resend (and what packets in particular)
+             
+                        First, I need to convert the string (the ACK) into a list of strings (each item is the ID as a string)
+                        Then, convert every item on the list from a string to an int. At this point, I can do stuff with it
+                        '''
+                        packetsToResend = ACK
+                        packetsToResend = packetsToResend.strip('[,]')
+                        packetsToResend = packetsToResend.replace(",","")
+                        packetsToResend = packetsToResend.replace(",","")
+                        packetsToResend = packetsToResend.split()
+                        packetsToResend = [int(i) for i in packetsToResend]
+                        numResent = 0
+                        while (numResent < len(packetsToResend)):
+                            '''
+                            Breakdown/format of lists:
+                            packetQueue:
+                                Index = id of each packet
+                                Object = data chunk associated with that ID
+                            packetsToResend:
+                                Index = unimportant (just the order that packets we dropped, but not relevant)
+                                Object = ID of a packet we didn't receive
+                            numResent (not a list):
+                                keeps track of what packet we're resending from the packetsToResend list
+                            What happens below:
+                                Assume 20 is the number of packets to resend
+                                We send the 0th to the 19th (20 total) packets - tracked using numResent
+                                We know which packet to use based on the object of the packetsToResend list
+                                We then know what data to send based on the object of the packetQueue list.
+                            '''
+                            if(s.sendto(packetQueue[packetsToResend[numResent]],addr) and s.sendto(str(packetsToResend[numResent]),addr)):
+                                print "Resent packet number: " + str(packetsToResend[numResent])
+                                numResent+=1
+                        else:
+                            '''
+                            We've resent the packets he asked for again, now we wait to make sure he's got them
+                            '''
+                            ACK = conn.recv(tcp_buf)
+                    else:
+                        '''
+                        He got all of our chunks, let's continue sending things
+                        '''
+                        #print "client got our chunks"
+                        numSent = 0
+                        loop += 1
+                if(s.sendto(chunk,addr) and s.sendto(str(local_ID),addr)):
+                    packetQueue[local_ID - (loop * howManyToSend)] = chunk
+                    numSent += 1
+                    local_ID += 1
+                    totalNumSent += 1
+    end = time.time()
+    s.close()      
+    f.close()
+    conn.close()
+    TCP_sock.close()
+    print "Total Num Sent: ",totalNumSent
+    print "File sent!"
+    print "Time: " + str(round(end-start,2)) + "s"
+    fsize = os.path.getsize("file.txt")/10e8
+    rate = fsize/(end-start)
+    print "Rate: " + str(round(rate,3)) +" GB/s = " + str(round(rate*8,3))+" Gb/s"
+    print "Loops: ", loop
+
+if (__name__ == '__main__'):
+    run()
